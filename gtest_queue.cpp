002b0308eabf531629e0c25bf7c1add0268ab851
Pierre Ramet <pierre.ramet@inria.fr>
2019-10-25 11:15:27 +0200

    add threadsafe test

--- gtest_queue.cpp
+++ gtest_queue.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
 #include "gtest/gtest.h"
 
@@ -18,16 +19,9 @@ void myfree(gpointer p) { free(p); }
 
 class QueueTest : public ::testing::Test {
  protected:
-  void SetUp() override {
-    q = queue_init(myfree); // queue_init(NULL);
-    for (int i=0;i<100;i++) {
-      tab[i]=(MYTYPE *)malloc(sizeof(MYTYPE));
-      *(tab[i])=(MYTYPE)i;
-    }
-  }
+  void SetUp() override { q = queue_init(myfree); } // queue_init(NULL);
   void TearDown() override { EXPECT_NO_CRASH(queue_free(q)); }
   queue_t* q;
-  MYTYPE* tab[100];
 };
 
 /* ********** TEST INIT & FREE ********** */
@@ -38,7 +32,9 @@ TEST_F(QueueTest, Init_Free) { EXPECT_FALSE(NULL == q); }
 
 TEST_F(QueueTest, Push) {
   for (int i = 0; i < 100; i++) {
-    queue_push_head(q, tab[i]);
+    MYTYPE *ptr_in = (MYTYPE *) malloc(sizeof(MYTYPE));
+    *ptr_in = i;
+    queue_push_head(q, ptr_in);
     MYTYPE* ptr_head = (MYTYPE*) queue_peek_head(q);
     EXPECT_EQ(*ptr_head, (MYTYPE)i);
     MYTYPE* ptr_tail = (MYTYPE*) queue_peek_tail(q);
@@ -49,18 +45,26 @@ TEST_F(QueueTest, Push) {
 /* ********** TEST POP ********** */
 
 TEST_F(QueueTest, Pop) {
-  for (int i = 0; i < 100; i++) queue_push_head(q, tab[i]);
   for (int i = 0; i < 100; i++) {
-    MYTYPE* ptr_v = (MYTYPE*) queue_pop_tail(q);
-    EXPECT_EQ(*ptr_v, (MYTYPE)i);
-    myfree(ptr_v);
+    MYTYPE *ptr_in = (MYTYPE *) malloc(sizeof(MYTYPE));
+    *ptr_in = i;
+    queue_push_head(q, ptr_in);
+  }
+  for (int i = 0; i < 100; i++) {
+    MYTYPE* ptr_out = (MYTYPE*) queue_pop_tail(q);
+    EXPECT_EQ(*ptr_out, (MYTYPE)i);
+    myfree(ptr_out);
   }
 }
 
 /* ********** TEST DROP ********** */
 
 TEST_F(QueueTest, Drop) {
-  for (int i = 0; i < 100; i++) queue_push_head(q, tab[i]);
+  for (int i = 0; i < 100; i++) {
+    MYTYPE *ptr_in = (MYTYPE *) malloc(sizeof(MYTYPE));
+    *ptr_in = i;
+    queue_push_head(q, ptr_in);
+  }
   for (int i = 0; i < 100; i++) queue_drop_tail(q);
   EXPECT_EQ(queue_length(q), 0);
 }
@@ -70,7 +74,11 @@ TEST_F(QueueTest, Drop) {
 
 TEST_F(QueueTest, Length) {
   EXPECT_EQ(queue_length(q), 0);
-  for (int i = 0; i < 10; i++) queue_push_head(q, tab[i]);
+  for (int i = 0; i < 10; i++) {
+    MYTYPE *ptr_in = (MYTYPE *) malloc(sizeof(MYTYPE));
+    *ptr_in = i;
+    queue_push_head(q, ptr_in);
+  }
   EXPECT_EQ(queue_length(q), 10);
   for (int i = 0; i < 10; i++) myfree(queue_pop_tail(q));
   EXPECT_EQ(queue_length(q), 0);
@@ -79,10 +87,49 @@ TEST_F(QueueTest, Length) {
 /* ********** TEST LENGTH ********** */
 
 TEST_F(QueueTest, Empty) {
-  for (int i = 0; i < 10; i++) queue_push_head(q, tab[i]);
+  for (int i = 0; i < 10; i++) {
+    MYTYPE *ptr_in = (MYTYPE *) malloc(sizeof(MYTYPE));
+    *ptr_in = i;
+    queue_push_head(q, ptr_in);
+  }
   EXPECT_FALSE(queue_is_empty(q));
   for (int i = 0; i < 10; i++) myfree(queue_pop_tail(q));
   EXPECT_TRUE(queue_is_empty(q));
+}
+
+/* ********** TEST THREADSAFE ********** */
+
+#define THREADS 8
+
+typedef struct {
+    int threadnumber;
+    queue_t *q;
+} thread_context_t;
+
+static thread_context_t contexts[THREADS];
+static pthread_t threads[THREADS];
+
+static void* thread_func(void* context)
+{
+  thread_context_t* ctx = (thread_context_t*) context;
+  for (int i = 0; i < 100; i++) {
+    MYTYPE *ptr_in = (MYTYPE *) malloc(sizeof(MYTYPE));
+    queue_push_head(ctx->q, ptr_in);
+    if (!queue_is_empty(ctx->q)) queue_drop_tail(ctx->q);
+  }
+  return NULL;
+}
+
+TEST_F(QueueTest, ThreadSafe) {
+  for (int i = 0; i < THREADS; i++) {
+    thread_context_t* ctx = contexts + i;
+    ctx->threadnumber = i + 1;
+    ctx->q = q;
+    pthread_create(&threads[i], NULL, &thread_func, ctx);
+  }
+  for(int i = 0; i < THREADS; i++) {
+    pthread_join(threads[i], NULL);
+  }
 }
 
 /* ********** MAIN ROUTINE ********** */
